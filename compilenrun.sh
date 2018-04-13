fcd456bfe2925b4ec6fadc1c524f313884f591b6
cb1711 <chaitanya.bhatia.cse15@iitbhu.ac.in>
2018-04-13 20:19:35 +0530

    Minor changes

--- compilenrun.sh
+++ compilenrun.sh
@@ -3,5 +3,5 @@ nvcc -arch=sm_35 -Xcompiler -fopenmp -O2 spmm1.cu -lcurand -o spmm.out
 echo "Compilation done!"
 echo "Now enter the input file"
 read file
-./spmm.out < ../$file
+./spmm.out < Datasets/$file
 exit 0
